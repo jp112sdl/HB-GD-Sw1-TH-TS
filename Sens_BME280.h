85ae1eb03c25af547126bad9b485c4ee56416694
jp112sdl <jp112sdl@icloud.com>
2021-08-24 07:32:08 +0200

    initial commit

--- Sens_BME280.h
+++ Sens_BME280.h
@@ -2,13 +2,21 @@
 #ifndef _SENS_BME280_H_
 #define _SENS_BME280_H_
 
+<<<<<<< HEAD
 #include <Wire.h>
 #include <Sensors.h>
 #include <BME280I2C.h>
+=======
+
+
+#include <Sensors.h>
+#include <BME280Spi.h>
+>>>>>>> 2e1768f (initial commit)
 #include <EnvironmentCalculations.h>
 
 namespace as {
 
+<<<<<<< HEAD
 BME280I2C::Settings settings(
    BME280::OSR_X1, // Temperature Oversampling Rate (tempOSR): OSR Enum, default = OSR_X1
    BME280::OSR_X1, // Humidity Oversampling Rate (humOSR): OSR Enum, default = OSR_X1
@@ -18,6 +26,9 @@ BME280I2C::Settings settings(
    BME280::Filter_Off, // Filter (filter): Filter Enum, default = Filter_16
    BME280::SpiEnable_False // SPI Enable: SpiEnable Enum, default = false
 );
+=======
+BME280Spi::Settings settings(5);
+>>>>>>> 2e1768f (initial commit)
 
 class Sens_Bme280 : public Sensor {
 
@@ -27,6 +38,7 @@ class Sens_Bme280 : public Sensor {
   uint8_t   _humidity;
   uint16_t  _dewPoint;
 
+<<<<<<< HEAD
   BME280I2C _bme280;        // Default : forced mode, standby time = 1000 ms, Oversampling = pressure ×1, temperature ×1, humidity ×1, filter off
 
 public:
@@ -37,12 +49,26 @@ public:
 
     Wire.begin(); //ToDo sync with further I2C sensor classes
 
+=======
+  BME280Spi _bme280;        // Default : forced mode, standby time = 1000 ms, Oversampling = pressure ×1, temperature ×1, humidity ×1, filter off
+
+public:
+
+  Sens_Bme280 () : _temperature(0), _pressure(0), _pressureNN(0), _humidity(0), _dewPoint(0), _bme280(settings) {}
+
+  void init () {
+    SPI.begin();
+>>>>>>> 2e1768f (initial commit)
     uint8_t i = 10;
     while( (!_bme280.begin()) && (i > 0) ) {
       delay(100);
       i--;
     }
+<<<<<<< HEAD
   
+=======
+
+>>>>>>> 2e1768f (initial commit)
     switch(_bme280.chipModel()) {
      case BME280::ChipModel_BME280:
        _present = true;
@@ -51,8 +77,12 @@ public:
      default:
        DPRINTLN(F("BME280 sensor NOT OK"));
     }
+<<<<<<< HEAD
     
     _bme280.setSettings(settings);
+=======
+
+>>>>>>> 2e1768f (initial commit)
   }
 
   void measure (uint16_t height) {
